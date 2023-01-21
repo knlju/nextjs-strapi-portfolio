defa3d9989f1f7167cefdcebd7477119735e1d12
knlju <ljubisakn@gmail.com>
2023-01-21 22:54:27 +0100

    added project page

--- web/components/Projects.js
+++ web/components/Projects.js
@@ -1,34 +1,9 @@
-import {API_URL} from "@/config";
+import Project from "@/components/Project";
 
 export default function Projects({projects}) {
-  console.log({projects})
   return (
     <div>
       {projects?.map(pr => <Project project={pr} key={pr.id}/>)}
-    </div>
-  )
-}
-
-export function Project({project}) {
-  const {attributes} = project
-  return (
-    <div>
-      <div>
-        {attributes.name}
-      </div>
-      <div>
-        {attributes.description}
-      </div>
-      <div>
-        {attributes.technologies}
-      </div>
-      <div>
-        {attributes.slug}
-      </div>
-      <img
-        src={`${API_URL}${attributes.preview.data[0].attributes.url}`}
-        alt={attributes.description}
-      />
     </div>
   )
 }
--- web/pages/projects/[slug].js
+++ web/pages/projects/[slug].js
@@ -1,9 +1,24 @@
-const Slug = () => {
-  return (
-    <div>
+import {Layout} from "@/components/Layout";
+import {API_URL} from "@/config";
+import Project from "@/components/Project";
 
-    </div>
+const Slug = ({project}) => {
+  return (
+    <Layout>
+      <Project project={project} />
+    </Layout>
   )
+}
+
+export async function getServerSideProps({ query: { slug } }) {
+  const res = await fetch(`${API_URL}/api/projects?filters[slug][$eq]=${slug}&populate=*`)
+  const projects = await res.json()
+
+  return {
+    props: {
+      project: projects.data[0],
+    },
+  }
 }
 
 export default Slug
